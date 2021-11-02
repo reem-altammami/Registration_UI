9f8a5153f4e3841589419dfa2aea888d11d9f54f
Reem Altammami <reem.saud.ruh.k@tuwaiq.edu.sa>
2021-11-02 06:33:06 +0300

    .

--- app/src/main/java/com/example/registration/MainActivity.kt
+++ app/src/main/java/com/example/registration/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.registration
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.registration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +17,19 @@ class MainActivity : AppCompatActivity() {
 
         // when click button validation function invoke. This function validation for every required field
         binding.buttonRegistration.setOnClickListener { validation() }
+     hideKeyboard()
+
     }
 
+    // when user press Enter this function call handleKeyEvent to hide keyboard
+    fun hideKeyboard(){
+        binding.name.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+        binding.email.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+        binding.address.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+        binding.password.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+        binding.rePassword.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+        binding.birthDate.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+    }
     // this function for checking Name (it will calling by function "validation")
 
     fun validName() {
@@ -41,7 +56,7 @@ class MainActivity : AppCompatActivity() {
 
     fun validPassword() {
         if (binding.password.text.toString().isEmpty()) {
-            binding.showName.setText(getString(R.string.passwordAlert))
+            binding.showName.text = getString(R.string.passwordAlert)
         } else if (binding.password.text.toString() != binding.rePassword.text.toString()) {
             binding.showName.text = getString(R.string.passAlert)
         } else {
@@ -64,6 +79,16 @@ class MainActivity : AppCompatActivity() {
             binding.welcom.text = getString(R.string.welcomeMasseg)
             binding.showName.setText(binding.name.text.toString())
         }
+    }
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
 
